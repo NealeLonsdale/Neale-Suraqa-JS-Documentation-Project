3ef7b3ebf36d7df58101392d5457683a5f06faba
NealeLonsdale <nealelons101@gmail.com>
2020-06-15 01:36:24 +0200

    finals

--- jsDocumentation/Babel.js
+++ jsDocumentation/Babel.js
@@ -1,4 +1,7 @@
 // CODED BY NEALE Lonsdale and Suraqa Jafrani.
 
-const projectName = 'Neale-Suraqa-Project';
-localStorage.setItem(js Documentation-Project);
+
+NealeLonsdale-HTML-CSS--Project;
+
+
+
